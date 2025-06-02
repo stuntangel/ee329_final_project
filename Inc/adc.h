3e53022ad9a4e2f10b10c840dee995e371e7e142
AJ Gregory <aj@whirlingdervii.com>
2025-06-02 16:42:38 -0700

    Merged Ryan WIP branch to AJ WIP branch.

--- Inc/adc.h
+++ Inc/adc.h
@@ -1,8 +1,36 @@
-#include "stm32l4xx_hal.h"
-#include "stm32l4xx_it.h"
-#define FALSE 0
-#define TRUE (!FALSE)
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : ADC.h
+  * @brief          : Header for ADC.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
 
-uint32_t grabConvertedResult( void );
-uint8_t checkFlag( void );
-void ADC_init( void );
+#ifndef INC_ADC_H_
+#define INC_ADC_H_
+
+// Module Includes
+#include "main.h"
+#include "delay.h"
+
+// Function Prototypes
+void ADC_Init(void);
+void ADC1_2_IRQHandler(void);
+uint32_t ADC_Read(void);
+
+// Global flag
+extern uint8_t  ADC_CONV_RDY;
+
+#endif /* INC_ADC_H_ */
